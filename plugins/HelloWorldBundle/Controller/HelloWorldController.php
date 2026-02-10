6d1729c7df702b20dee2388439c39637093bf101
Matt Lima <matt@3-tone.com>
2026-02-10 13:56:01 -0500

    add db export bundle

--- plugins/HelloWorldBundle/Controller/HelloWorldController.php
+++ plugins/HelloWorldBundle/Controller/HelloWorldController.php
@@ -2,17 +2,26 @@
 
 namespace KimaiPlugin\HelloWorldBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
-class HelloWorldController extends AbstractController
+class HelloWorldController
 {
+    private Environment $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     public function index(): Response
     {
         $loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
         
-        return $this->render('@HelloWorld/hello.html.twig', [
+        $content = $this->twig->render('@HelloWorld/hello.html.twig', [
             'content' => $loremIpsum,
         ]);
+        
+        return new Response($content);
     }
 }
